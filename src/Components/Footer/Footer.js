28d18945d068a4b0b1e92705910bd7a5620e581b
mma-tahmid <mma.tahmid@gmail.com>
2023-06-06 14:00:46 +0600

    added header

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -23,8 +23,8 @@ const Footer = () => {
                     </div>
 
 
-                    <div >
-                        <img className="img-fluid w-25" src="https://jobguidezone.com/wp-content/uploads/2021/06/Top-Mobile-Banking-In-Bangladesh-2021-1200x759.jpg" alt="" />
+                    <div className='footer_image' >
+                        <img className="img-fluid w-75" src="https://media.istockphoto.com/id/1271752802/photo/phone-and-e-mail-icons-on-wooden-cubes-with-contact-us-text-on-blue-background-web-page.jpg?s=612x612&w=0&k=20&c=dk9oPaDy_L9mv_icOMgsFGzEDrX0NUI3I8xBQ-DAxQM=" alt="" />
                     </div>
 
 
